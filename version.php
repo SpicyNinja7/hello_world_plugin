4d7f457318da1fcfb093290db4b64950c82db992
Is Faid <faid.aznam@gmail.com>
2024-05-11 22:50:12 +0800

    update

--- version.php
+++ version.php
@@ -26,6 +26,6 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'block_helloworld';
 $plugin->release = '0.1.0';
-$plugin->version = 2024050800;
+$plugin->version = 2024051100;
 $plugin->requires = 2022112800;
 $plugin->maturity = MATURITY_ALPHA;
