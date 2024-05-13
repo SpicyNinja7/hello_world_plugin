5bb66ad4cd42693f03d0346d9044764000a06de9
Is Faid <faid.aznam@gmail.com>
2024-05-14 06:38:01 +0800

    Revert "update"
    This reverts commit a0e3291557779a403ec4b5db86eb084a622483c4.

--- version.php
+++ version.php
@@ -21,17 +21,11 @@
  * @copyright   2024 Is Faid Aznam <isfaid.aznam@gmail.com>
  * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
+
 defined('MOODLE_INTERNAL') || die();
 
-$plugin = new stdClass();
 $plugin->component = 'block_helloworld';
-$plugin->version = 2024051300;
-$plugin->requires = 2023020600;
 $plugin->release = '0.1.0';
+$plugin->version = 2024051300;
+$plugin->requires = 2022061400;
 $plugin->maturity = MATURITY_ALPHA;
-
-const MATURITY_ALPHA = 'alpha';
-const MATURITY_BETA = 'beta';
-const MATURITY_RC = 'rc';
-const MATURITY_STABLE = 'stable';
-?>
