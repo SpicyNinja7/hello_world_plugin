fe2fa71086d8ed7c1022cbda353ba458ea5d6c78
Is Faid <faid.aznam@gmail.com>
2024-05-14 06:38:06 +0800

    Revert "update"
    This reverts commit fc63eebe3f9f4bec3fbd0a2eb3915ca1fcfc4966.

--- version.php
+++ version.php
@@ -26,6 +26,6 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'block_helloworld';
 $plugin->release = '0.1.0';
-$plugin->version = 2024051300;
+$plugin->version = 2024051100;
 $plugin->requires = 2022061400;
 $plugin->maturity = MATURITY_ALPHA;
